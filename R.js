8691aa3983db57881ee6c588ce076b890f2d589a
sagomilk <sagomilk@gmail.com>
2014-08-02 20:50:27 +0800

    median function

--- R.js
+++ R.js
@@ -3,6 +3,24 @@
 	var _R = global.R,
 		R = {};
 
+	// referer https://gist.github.com/caseyjustus/1166258
+	function median(values) { 
+	    values.sort(function(a,b) {
+	    	return a - b;
+	    });
+	 
+	    var half = Math.floor(values.length/2);
+	 
+	    if(values.length % 2) {
+	        return values[half];
+	    } else {
+	        return (values[half-1] + values[half]) / 2.0;
+	    }
+	}
+	R.median = median;
+
+
+
 	// expose to global namespace
 	global.R = R;
 
