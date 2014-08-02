78e99f53e61f7fb07a88af147e0bf170d6bc6ce2
sagomilk <sagomilk@gmail.com>
2014-08-02 22:31:27 +0800

    function | mean min max

--- R.js
+++ R.js
@@ -20,6 +20,41 @@
 	R.median = median;
 
 
+	// referer http://javascript.about.com/library/blaravg.htm
+	function mean(values) {
+		var avg = 0,
+			count = 0,
+			length = values.length;
+
+		for (var i = 0; i < length; i++) {
+			var e = +values[i];
+			if(!e && values[i] !== 0 && values[i] !== '0') {
+				e--;
+			}
+
+			if (values[i] == e) {
+				avg += e; 
+				count++;
+			}
+		}
+		
+		return avg/count;
+	}
+	R.mean = mean;
+	//	use case
+	//	var ary = [5,8,2,4,11,7,3];
+	//	var average = ary.avg();
+
+
+	function min(values) {
+		return Math.min.apply(null, values);
+	}
+	R.min = min;
+
+	function max(values) {
+		return Math.max.apply(null, values);
+	}
+	R.max = average;
 
 	// expose to global namespace
 	global.R = R;
